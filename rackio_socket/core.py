3c79f3cb00672ba3a6d00df85c4a2769ddea1544
Nelson Carrasquel <carrasquel@outlook.com>
2020-09-06 10:27:38 -0400

    Server integration into Rackio App

--- rackio_socket/core.py
+++ rackio_socket/core.py
@@ -7,10 +7,24 @@ This module implements the core app class and methods for Rackio Socket.
 import json
 
 from ._singleton import Singleton
+from .worker import SocketWorker
+
+from .decorator import AppendWorker
 
 
 class SwaggerSocket(Singleton):
 
     def __init__(self):
 
-        pass
+        super(SwaggerSocket, self).__init__()
+
+        self.app = None
+        self.worker = None
+
+    def __call__(self, app, port):
+
+        self.app = app
+        self.worker = SocketWorker(port)
+
+        app._start_workers = AppendWorker(app._start_workers, self.worker)
+
