d2e61ca4e9db50e0c7baf86eacfffa0b3af9feaf
crivero7 <cdrr.rivero@gmail.com>
2020-10-05 13:14:00 -0400

    Client example

--- rackio_socket/events.py
+++ rackio_socket/events.py
@@ -11,6 +11,7 @@ rs = SocketCore()
 @rs.event
 def connect(sid, environ):
     print("connect " , sid)
+    rs.emit("welcome", {"message": "Welcome to RackioSocket, the SocketIO server for Rackio!"})
 
 @rs.event
 def disconnect(sid, environ):
