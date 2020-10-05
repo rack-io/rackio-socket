9066175d7f45266c96c0ad8ddb85a8f7f598011b
crivero7 <cdrr.rivero@gmail.com>
2020-10-05 15:37:09 -0400

    Tags dao completed

--- rackio_socket/events.py
+++ rackio_socket/events.py
@@ -34,4 +34,49 @@ def tags(sid):
 
     tags = TagsDAO()
 
-    return tags.get_all()    
+    return tags.get_all()
+
+
+@rs.event
+def write_tag(sid, data):
+
+    tag_id = data["tag_id"]
+    value = data["value"]
+
+    tags = TagsDAO()
+
+    return tags.write(tag_id, value)
+
+
+@rs.event
+def tag_history(sid, data):
+
+    tag_id = data["tag_id"]
+
+    tags = TagsDAO()
+
+    return tags.get_history(tag_id)
+
+
+@rs.event
+def tag_trend(sid, data):
+
+    tag_id = data["tag_id"]
+    tstart = data["tstart"]
+    tstop = data["tstop"]
+
+    tags = TagsDAO()
+
+    return tags.get_trend(tag_id, tstart, tstop)
+
+
+@rs.event
+def tag_trends(sid, data):
+
+    tags = data["tags"]
+    tstart = data["tstart"]
+    tstop = data["tstop"]
+
+    tags = TagsDAO()
+
+    return tags.get_trends(tags, tstart, tstop)
