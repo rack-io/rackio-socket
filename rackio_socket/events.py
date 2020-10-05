e17f3df465adbf32701b14446087478b3615525f
crivero7 <cdrr.rivero@gmail.com>
2020-10-05 15:03:53 -0400

    tags and dbtags added

--- rackio_socket/events.py
+++ rackio_socket/events.py
@@ -3,17 +3,35 @@
 
 This module implements all socket events defined for Rackio Socket.
 """
-
+from rackio.dao import *
 from .core import SocketServer
 
 server = SocketServer()
 rs = server.sio
+
 
 @rs.event
 def connect(sid, environ):
     print("connect " , sid)
     rs.emit("welcome", {"message": "Welcome to RackioSocket, the SocketIO server for Rackio!"})
 
+
 @rs.event
 def disconnect(sid):
     print("disconnect " , sid)
+
+
+@rs.event
+def dbtags(sid):
+
+    logger = LoggerDAO()
+
+    return logger.get_all()
+
+
+@rs.event
+def tags(sid):
+
+    tags = TagsDAO()
+
+    return tags.get_all()    
